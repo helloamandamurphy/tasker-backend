79c0deb5a7b1a01cba423892a119f7dcaa0507fe
Amanda Murphy <helloamandamurphy@gmail.com>
2020-02-16 15:25:27 -0500

    Add User has_many :tasks, through: :lists to User model for better functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :lists
+  has_many :tasks, through: :lists
 
   validates :name, :email, presence: true
   validates :email, uniqueness: true
