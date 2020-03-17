3c54b368eb97836457ea2f006ac781fbc0a0a78f
Amanda Murphy <helloamandamurphy@gmail.com>
2020-03-17 17:05:07 -0400

    Update Users#create to include conditional statement to set session id and render user page if the new user saves or render errors if the user info does not save

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,8 +11,15 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    render json: @user, status: 200
+    if @user.save
+      session[:user_id] = @user.id
+      render json: @user, status: :created
+    else
+      resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
   end
 
   # def update
@@ -26,4 +33,10 @@ class Api::V1::UsersController < ApplicationController
   #
   #   render json: {userId: @user.id}
   # end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
