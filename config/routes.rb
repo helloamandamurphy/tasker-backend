0051051081590de50cbf354192c3d2210c6cdf07
Amanda Murphy <helloamandamurphy@gmail.com>
2020-03-17 17:05:52 -0400

    Add sign up route and comments to other routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  #Signup
+  post '/api/v1/signup', to: 'api/v1/users#create'
+  #Login
   post '/api/v1/login', to: 'api/v1/sessions#create'
+  #Logout
   delete '/api/v1/logout', to: 'api/v1/sessions#destroy'
+  #Set Current User
   get '/api/v1/get_current_user', to: 'api/v1/sessions#get_current_user'
-  
+
 
   namespace :api do
     namespace :v1 do
